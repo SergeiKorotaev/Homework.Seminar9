577b33828403714eedcf7df23b59ce9df9fe786a
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-02-06 08:52:19 +0300

    test2

---
+++ Test2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число M:");
+int numberM = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+void Sum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма чисел от M до N = {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    Sum(numberM, numberN, sum);
+}
+
+Sum(numberM, numberN, 0);;
