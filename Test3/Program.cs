f2ff70689ce44838bba5fe29181798c6b6efc624
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-02-06 08:57:58 +0300

    test3

---
+++ Test3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+int AckkerFun(int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckkerFun(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckkerFun(numberM - 1, AckkerFun(numberM, numberN - 1));
+return AckkerFun(numberM, numberN);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({numberM}, {numberN}) = {AckkerFun(numberM, numberN)}");
