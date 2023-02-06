9f1e55d30c534b5fe5dc1a6ccb3f5a0ee00a93f5
Sergei Korotaev <serega_korotaev24@gmail.ru>
2023-02-06 08:37:28 +0300

    test1

---
+++ Test1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число от 1:");
+int number = int.Parse(Console.ReadLine());
+
+void PrintNumbers (int number)
+{
+    if (number < 0) Console.Write("Неверныйй ввод");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    PrintNumbers (number - 1);
+}
+
+PrintNumbers(number);
